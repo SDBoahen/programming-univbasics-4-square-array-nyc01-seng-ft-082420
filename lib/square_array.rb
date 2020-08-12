eafbee8a9853e520ced06dc7eb1c987720b438e7
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-12 18:49:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,40 @@
-def square_array(array)
+
+
+lab_nums = 
+[
+  
+  1, 2, 3 
+  
+]
+
+
+
+
+def 
+  
+  square_array(array)
   # your code here
+  
+  count = 0
+  
+  sq_array = Array.new
+  
+  
+  while count < (array.length - 1)
+  
+    sq_array[count] = array[count] * array[count]
+  
+    p square_array
+  
+  
+    count +=
+  
+  end
+  
+  
 end
+
+
+
+square_array(lab_nums)
+
